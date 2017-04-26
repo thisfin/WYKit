2e1ad6e9b49a075adfa3b42a40b1c5956aa81e31
文优 <thisfin@gmail.com>
2017-04-26 17:45:55 +0800

    Update

---
+++ WYKit.podspec
@@ -0,0 +1,44 @@
+#
+# Be sure to run `pod lib lint WYKit.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = 'WYKit'
+  s.version          = '0.0.1'
+  s.summary          = 'wen you\'s dev tools'
+
+# This description is used to generate tags and improve search results.
+#   * Think: What does it do? Why did you write it? What is the focus?
+#   * Try to keep it short, snappy and to the point.
+#   * Write the description between the DESC delimiters below.
+#   * Finally, don't worry about the indent, CocoaPods strips it!
+
+  s.description      = <<-DESC
+TODO: Add long description of the pod here.
+                       DESC
+
+  s.homepage         = 'https://github.com/thisfin/WYKit'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { '文优' => 'thisfin@gmail.com' }
+  s.source           = { :git => 'https://github.com/thisfin/WYKit.git', :tag => s.version.to_s }
+  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+
+  s.ios.deployment_target = '8.0'
+  s.osx.deployment_target = '10.12'
+
+  s.source_files = 'WYKit/Classes/**/*'
+  s.requires_arc = true
+  
+  # s.resource_bundles = {
+  #   'WYKit' => ['WYKit/Assets/*.png']
+  # }
+
+  # s.public_header_files = 'Pod/Classes/**/*.h'
+  # s.frameworks = 'UIKit', 'MapKit'
+  # s.dependency 'AFNetworking', '~> 2.3'
+end
---
+++ WYKit/Classes/WYIconfont.swift
@@ -0,0 +1,142 @@
+//
+//  WYIconfont.swift
+//  SwiftQRCodeScan
+//
+//  Created by wenyou on 2016/11/8.
+//  Copyright © 2016年 wenyou. All rights reserved.
+//
+
+import Foundation
+import CoreText
+
+#if os(iOS)
+    import UIKit
+    public typealias WYColor = UIColor
+    public typealias WYFont = UIFont
+    public typealias WYImage = UIImage
+#else
+    import AppKit
+    public typealias WYColor = NSColor
+    public typealias WYFont = NSFont
+    public typealias WYImage = NSImage
+#endif
+
+
+class WYIconfont: NSObject {
+    private static var fontName = "FontAwesome"
+    private static var fontPath = "fontawesome-webfont_4.6.3"
+
+    // once范例
+    private static var oneTimeThing: () = {
+        let frameworkBundle: Bundle = Bundle(for: WYIconfont.classForCoder())
+        let path: String? = frameworkBundle.path(forResource: WYIconfont.fontPath, ofType: "ttf")
+        if let dynamicFontData = NSData(contentsOfFile: path!) {
+            let dataProvider: CGDataProvider? = CGDataProvider(data: dynamicFontData)
+            let font: CGFont? = CGFont(dataProvider!)
+            var error: Unmanaged<CFError>? = nil
+
+            if !CTFontManagerRegisterGraphicsFont(font!, &error) {
+                let errorDescription: CFString = CFErrorCopyDescription(error!.takeUnretainedValue())
+                NSLog("Failed to load font: %@", errorDescription as String)
+            }
+            error?.release()
+        }
+    }()
+
+    // MARK: - public
+    public static func setFont(fontPath: String, fontName: String) {
+        WYIconfont.fontPath = fontPath
+        WYIconfont.fontName = fontName
+    }
+
+    public static func fontOfSize(_ fontSize: CGFloat) -> WYFont {
+        _ = oneTimeThing
+
+        let font: WYFont? = WYFont(name: WYIconfont.fontName, size: fontSize)
+        assert(font != nil, WYIconfont.fontName + " couldn't be loaded")
+        return font!
+    }
+
+    public static func imageWithIcon(content: String, backgroundColor: WYColor = WYColor.clear, iconColor: WYColor = WYColor.white, size: CGSize) -> WYImage {
+        // 逐步缩小算字号
+        var fontSize: Int!
+        let constraintSize = CGSize(width: size.width, height: CGFloat(MAXFLOAT))
+        for i in stride(from: 500, to: 5, by: -2) {
+            let rect = content.boundingRect(with: constraintSize,
+                                            options: NSStringDrawingOptions.usesFontLeading,
+                                            attributes: [NSFontAttributeName: WYIconfont.fontOfSize(CGFloat(i))],
+                                            context: nil)
+            fontSize = i
+            if rect.size.height <= size.height {
+                break
+            }
+        }
+
+        #if os(iOS)
+            // 绘制
+            let textRext = CGRect(origin: CGPoint.zero, size: size)
+            UIGraphicsBeginImageContextWithOptions(size, false, 0)
+            backgroundColor.setFill()
+            UIBezierPath(rect: textRext).fill()
+            content.draw(in:textRext, withAttributes: [NSFontAttributeName: WYIconfont.fontOfSize(CGFloat(fontSize)),
+                                                       NSForegroundColorAttributeName: iconColor,
+                                                       NSBackgroundColorAttributeName: backgroundColor,
+                                                       NSParagraphStyleAttributeName: {
+                                                        let style = NSMutableParagraphStyle()
+                                                        style.alignment = NSTextAlignment.center
+                                                        return style}()])
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return image!
+        #else
+            // 绘制
+            let textRext = NSRect(origin: NSPoint.zero, size: size)
+            let image = NSImage(size: size)
+            image.lockFocus()
+            //        let context : CGContext = (NSGraphicsContext.current()?.cgContext)!
+            //        context.beginPath()
+            backgroundColor.setFill()
+            NSBezierPath(rect: textRext).fill()
+            content.draw(in:textRext, withAttributes: [NSFontAttributeName: WYIconfont.fontOfSize(CGFloat(fontSize)),
+                                                       NSForegroundColorAttributeName: iconColor,
+                                                       NSBackgroundColorAttributeName: backgroundColor,
+                                                       NSParagraphStyleAttributeName: {
+                                                        let style = NSMutableParagraphStyle()
+                                                        style.alignment = NSTextAlignment.center
+                                                        return style}()])
+            image.unlockFocus()
+            return image
+        #endif
+    }
+
+    public static func imageWithIcon(content: String, backgroundColor: WYColor = WYColor.clear, iconColor: WYColor = WYColor.white, fontSize: CGFloat) -> WYImage {
+        let attributes = [NSFontAttributeName: WYIconfont.fontOfSize(fontSize),
+                          NSForegroundColorAttributeName: iconColor,
+                          NSBackgroundColorAttributeName: backgroundColor,
+                          NSParagraphStyleAttributeName: {
+                            let style = NSMutableParagraphStyle()
+                            style.alignment = NSTextAlignment.center
+                            return style}()]
+        #if os(iOS)
+            var size = content.size(attributes: attributes)
+            size = CGSize(width: size.width * 1.1, height: size.height * 1.05)
+            UIGraphicsBeginImageContextWithOptions(size, false, 0)
+            backgroundColor.setFill()
+            UIBezierPath(rect: CGRect(origin: CGPoint.zero, size: size)).fill()
+            content.draw(at: CGPoint(x: size.width * 0.05, y: size.height * 0.025), withAttributes: attributes)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return image!
+        #else
+            var size = content.size(withAttributes: attributes)
+            size = NSMakeSize(size.width * 1.1, size.height * 1.05)
+            let image = NSImage(size: size)
+            image.lockFocus()
+            backgroundColor.setFill()
+            NSBezierPath(rect: NSRect(origin: NSPoint.zero, size: size)).fill()
+            content.draw(at: NSMakePoint(size.width * 0.05, size.height * 0.025), withAttributes: attributes)
+            image.unlockFocus()
+            return image
+        #endif
+    }
+}
